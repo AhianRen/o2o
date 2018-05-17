b71f4be6ad5108e6963039fc906596cbb0c902da
ahian <yahong.ren@foxmail.com>
2018-05-17 17:16:38 +0800

    加入logback配置

--- src/main/java/com/zb/o2o/web/superadmin/AreaController.java
+++ src/main/java/com/zb/o2o/web/superadmin/AreaController.java
@@ -2,6 +2,8 @@ package com.zb.o2o.web.superadmin;
 
 import com.zb.o2o.entity.Area;
 import com.zb.o2o.service.AreaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +17,14 @@ import java.util.Map;
 @Controller
 @RequestMapping("/superadmin")
 public class AreaController {
+    Logger logger = LoggerFactory.getLogger(AreaController.class);
     @Autowired
     private AreaService areaService;
     @RequestMapping(value = "/listarea",method = RequestMethod.GET)
     @ResponseBody
     private Map<String,Object> listareas(){
+        logger.info("=========atart=======");
+        long startTime = System.currentTimeMillis();
         Map<String,Object> modelMap = new HashMap<>();
         try {
             List<Area> areaList = areaService.getAreaList();
@@ -30,6 +35,10 @@ public class AreaController {
             modelMap.put("success",false);
             modelMap.put("errMsg",e.toString());
         }
+        logger.error("test error");
+        long endTime = System.currentTimeMillis();
+        logger.debug("costTime:[{}ms]",endTime-startTime);
+        logger.info("=======end============");
         return modelMap;
     }
 }
